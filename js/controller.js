d6994ac5848996d574770a63e1bf981a64f15a20
Kashmir Kalirai <kashmirk67@gmail.com>
2017-10-22 18:01:19 +0100

    responsive

--- js/controller.js
+++ js/controller.js
@@ -1,87 +1,76 @@
 angular.module('RouteControllers', [])
-  //  .controller('HomeController', function($scope) {
-  //      $scope.title = "Charles Martin Swing Band!";
-  //  })
-
-    .controller('AudioController', function($scope) {
+  
+    .controller('AudioController', function($scope, $sce) {
         $scope.title = "Soundclips from back catalogue";
-        //pause 1 audio to play another?
-        document.addEventListener('play', function(e){
-        var audios = document.getElementsByTagName('audio');
-        for(var i = 0, len = audios.length; i < len;i++){
-            if(audios[i] != e.target){
-                audios[i].pause();
-            }
-            }
-        }, true);
+        //create object to hold sound clips:
+        $scope.objSoundClips = [
+                {track:"Topsy", album:"Modern Times", filemp3:"/static/music/c5ztopsy.mp3", fileogg:"/static/music/c5ztopsy.ogg"},
+                {track:"Moon River", album:"Rain In June", filemp3:"/static/music/c3zmoonriver.mp3", fileogg:"/static/music/c3zmoonriver.ogg"},
+                {track:"Black Coffee", album:"Modern Times", filemp3:"/static/music/c5vblackcoffee.mp3", fileogg:"/static/music/c5vblackcoffee.ogg"},
+                {track:"Fish for Supper", album:"Fancy Cocktails and Hot Sauce", filemp3:"/static/music/c5zfish.mp3",fileogg:"/static/music/c5zfish.ogg"}
+                ];
+        
+       $scope.TrustUrl = function(stringurl) {
+               //alert(stringurl)
+            return $sce.trustAsResourceUrl(stringurl);
+        };
+
     })
+    
      .controller('VideoController', function($scope) {
         $scope.title = "See video clips from our back catalogue";
-        $scope.eventtype="Anniversary"
     })
+ 
     .controller('GalleryController', function($scope) {
         $scope.title = "Photos of the band members";
-        // move selected image to front, send rest to back????
-        $("#gallery1").click(function(){  
-            $("#gallery1").removeClass("image_to_back");
-            $("#gallery1").addClass("image_enlarge");
-            $("#gallery2").addClass("image_to_back");
-            $("#gallery2").removeClass("image_enlarge");
-            $("#gallery3").addClass("image_to_back");
-            $("#gallery3").removeClass("image_enlarge");
-            $("#gallery4").addClass("image_to_back");
-            $("#gallery4").removeClass("image_enlarge");
-            $("#gallery5").addClass("image_to_back");
-            $("#gallery5").removeClass("image_enlarge");
-            alert("test #gallery1 - click event");
-            $("#gallery2").css("z-index", 0);
-            $("#gallery1").css("z-index", 100);
+        //set defaults of all images to go to back:
+        $(".galleryitems").addClass("image_to_back").removeClass("image_enlarge");
+        
+        // move clicked on image to front, send rest to back, $(this).css('z-index', 999);:
+        $(".galleryitems").click(function(element){
+            $(this).removeClass("image_to_back").addClass("image_enlarge");
+            $(".galleryitems").not(this).addClass("image_to_back").removeClass("image_enlarge");
         });
     })
    
    .controller('BookingController', function($scope,$location) {
         $scope.title = "Booking Enquiry";
+        $scope.radioboxModel="day"; //set default value for radio buttons
         $scope.bookingCustomer = {};    //create new empty object
         var AvailableDates = ['20/12/2017', '21/12/2017','22/12/2017','23/12/2017'];
         //array for dropdown listbox for event type
         $scope.eventtypes = ["Birthday", "Wedding", "Anniversary", "Other"];
-            //wen user clicks on submit button check date entered against dates in array
+            //wen user clicks on submit button check customer date against available dates in array
         $scope.submitForm = function() {
             if ($scope.bookingForm.$valid) {
-                $scope.bookingCustomer.fullname = $scope.customer.fullname;
                 $scope.bookingCustomer.eventdate = $scope.customer.eventdate;
-                //alert($scope.radioboxModel.value);
+                alert($scope.radioboxModel);
                 //check event date entered matches 1 held in array AvailableDates
-                for (var stringdate in AvailableDates) {
-                    if ($scope.bookingCustomer.eventdate===AvailableDates[stringdate]) {
-                        if ($scope.radioboxModel.value = "evening") {
+                for (var i in AvailableDates) {
+                    if ($scope.bookingCustomer.eventdate===AvailableDates[i]) {
+                        if ($scope.radioboxModel == "evening") {
                             alert("Thank you for your query. Your chosen event date is available for booking. The estimated cost of this evening booking would be £1500.  We will be in touch shortly.");
-
-                        } else if ($scope.radioboxModel.value = "day") {
+                        } else if ($scope.radioboxModel == "day") {
                             alert("Thank you for your query. Your chosen event date is available for booking. The estimated cost of this day booking would be £3000.  We will be in touch shortly.");
                         }                 
-                        $location.path("/");
                         break;    
                     } 
                 }
                 if (AvailableDates.indexOf($scope.bookingCustomer.eventdate) === -1) {
-                    alert ("Sorry this date is not available. Please choose another date");
+                    alert ("Sorry this date is not available. The next available date is " + AvailableDates[0]);
                 }
             }    
-    	}
-        //reset package
-        $scope.radioboxModel.value = ""
+    	}   
     })
 
     .controller('MailingController', function($scope) {
-        $scope.title = "Charles Martin Swing Band!";
+        $scope.title = "Be the first to find out";
         $scope.mailingUser = {};
         $scope.submitForm = function() {
             if ($scope.mailingForm.$valid) {
                 $scope.mailingUser.fullname = $scope.user.fullname;
                 $scope.mailingUser.email = $scope.user.email;
+                alert("You have successfully subscribed to our mailing list.");
         	}
-        	console.log($scope.mailingUser.fullname + " " + $scope.mailingUser.email);
-        	alert("You have successfully subscribed to our mailing list.");
     	}
     })
